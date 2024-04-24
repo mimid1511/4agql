6052b99b3ef99f80ae34eddecfb5757330a22108
Mohamed ben <mimid1511@gmail.com>
2024-04-24 20:02:04 +0200

    som
    som

--- Front/4agql/app/Classes/page.js
+++ Front/4agql/app/Classes/page.js
@@ -2,24 +2,26 @@ import Grid from "@/Components/Grid";
 import { classeClient } from "@/lib/apolloClient";
 import { gql } from "@apollo/client";
 
-const query = gql`query {
-  classes {
-      _id
-      name
-      professorId
-      studentIds
-  }
-}`;
 
 export default async function Classes() {
 
-  const { data } = await classeClient().query({ query });
+  const { data } = await classeClient().query({query : gql`query {
+    userByEmail(email: "sss") {
+      _id
+      email
+      pseudo
+      password
+      role
+    }
+  }
+  `});
   console.log(data);
 
   return (
     <main>
       <section style={{ padding: 25 }}>
-        <Grid data={data}/>
+        <h1 class="mb-4  font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl dark:text-white">Classe :</h1>
+        <Grid data={data} />
       </section>
     </main>
   );
--- Front/4agql/app/ProfilUpdate/page.js
+++ Front/4agql/app/ProfilUpdate/page.js
@@ -1,24 +1,28 @@
-import { noteClient } from "@/lib/apolloClient";
+
+
+import { loginClient } from "@/lib/apolloClient";
 import { gql } from "@apollo/client";
 import { useAuth } from "@/Components/Provider";
+import { useSession } from "next-auth/react";
 
-
-const query = gql`query {
-  grades {
-    _id
-    studentId
-    teacherId
-    subjectId
-    value
-    coefficient
-  }
-}`;
 
 export default async function Profil() {
 
+  const { data: session, status } = useSession();
 
-  const { data } = await noteClient().query({ query });
-  console.log(data);
+  const { fetch } = await loginClient().query({query : gql`query {
+    userByEmail(email: "${session.user.email}") {
+      _id
+      email
+      pseudo
+      password
+      role
+    }
+  }
+  `});
+
+
+
 
 
   // const { isAuthenticated, user, logout } = useAuth();
@@ -30,14 +34,15 @@ export default async function Profil() {
   return (
     <main>
       <section style={{ padding: 25 }}>
+        <h1 class="mb-4  font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl dark:text-white">Modifier le profil</h1>
         <div style={{ width: '500px' }}>
           <label className="input input-bordered flex items-center gap-2">
             Pseudo
-            <input type="text" className="grow"/>
+            <input type="text" className="grow" />
           </label>
           <label className="input input-bordered flex items-center gap-2">
             Email
-            <input type="email" className="grow"/>
+            <input type="email" className="grow" />
           </label>
           <label className="input input-bordered flex items-center gap-2">
             Mot de passe
@@ -48,7 +53,7 @@ export default async function Profil() {
             <input disabled type="text" className="grow" />
           </label>
         </div>
-        <br/>
+        <br />
         <button className="btn">Mettre à jours le profil</button>
       </section>
     </main>
